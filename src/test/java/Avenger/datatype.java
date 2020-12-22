81fce7bcbcb152f64b390acac24050c400db506f
nahar20 <nahartx2019@gmail.com>
2020-12-22 00:37:42 -0600

    Lucky

---
+++ src/test/java/Avenger/datatype.java
@@ -0,0 +1,88 @@
+package Avenger;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		
+		// addition
+		int d= 4;
+		int c=4;
+		int sum = d+c;
+		System.out.println("total"+ sum);
+		
+		// modulus or remainder
+		int a=2;
+		double b=5.6;
+		String Fruits = "Lily";
+		double mud= a%b;
+		System.out.println(mud);
+		// subtraction
+         int sub= d-c;
+		System.out.println("total sub is"+sub);
+		
+		// multiplication
+		double mul=a*b;
+		
+		System.out.println("my multiplication is"+ mul);
+		
+		
+		//increment
+		//could be 1, 2 or 3 or depends on condition value increase by 1, 2 or 3
+		 
+		
+		int f= 4;
+		 //f++;// I want to increment by 1
+		 // or 
+		 f= f+1;// both are same
+		 System.out.println("increment by 1"+ f);
+		 
+		 // increment by 1 when value is 100
+		 int value1= 100;
+		 value1++;
+		 System.out.println("increment by 1"  + value1);
+		 
+		 // increment by 5 when value is 90
+		int value2= 90;
+		value2= value2+5;
+		System.out.println ("increment by 2 " +  value2);
+		
+		  // decrement by 1
+		 int e= 3;
+		 e--;
+		 System.out.println("decrement by1" + e);
+		 // decrement by 5
+		 
+		 int h= 20;
+		 h--;
+		 System.out.println("decrement by 5  "+ h);
+		  
+		 // decrement by 3
+		  int value3=30;
+		  //value3--;
+		  value3=value3-3;
+		  System.out.println(" decrement by 3 "+ value3);
+		  
+		  // increcrese by 10
+		  int value4= 100;
+		   value4=value4+10;
+		   System.out.println(" increcrese by 10 "+ value4);
+		  
+		  
+		  
+		  
+		  
+		 
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+ 
+}
